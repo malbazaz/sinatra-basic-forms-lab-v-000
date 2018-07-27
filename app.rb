37e1fdedaa62147ca646ce03166f5cfaccb06779
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-27 15:58:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,5 +10,10 @@ class App < Sinatra::Base
       erb: create_puppy
     end
 
+    post "/new" do
+      @name = params[:name]
+      @breed = params[:breed]
+      @age = params[:age]
+      erb: display_puppy
 
 end
